f02df6bdd7f46a2178e1113e98a3a3940c1f2f20
Baboo <philbond1997@hotmail.com>
2018-10-19 10:43:32 -0400

    Simplified StringReader

---
+++ src/model/Clsc.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Clsc {
+	
+	int id_;
+	boolean hasTerminal_;
+	ArrayList<Path> paths_;
+	
+	public Clsc(int id, boolean hasTerminal) {
+		id_ = id;
+		hasTerminal_ = hasTerminal;
+		paths_ = new ArrayList<Path>();
+	}
+	
+	public int getId() {
+		return id_;
+	}
+	
+	public boolean hasCharge() {
+		return hasTerminal_;
+	}
+	
+	void addPath(Path path) {
+		paths_.add(path);
+	}
+	
+	public String toString() {
+		return "CLSC" + id_ + ", " + hasTerminal_ + "\n";
+	}
+}
