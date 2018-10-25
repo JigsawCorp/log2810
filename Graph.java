9a9ba31c11fc556a1a52d55f608f7f63708d221f
Samuel Saito-Gagne <samuel.saito-gagne@polymtl.ca>
2018-10-24 21:55:26 -0400

    Continuation of findShortestPath in Graph

--- Graph.java
+++ Graph.java
@@ -19,31 +19,64 @@ public class Graph {
 				
 		// For each CLSC, we have 1) the path to get to that CLSC, starting from "start"
 		//					and	2) the time it took us to get to that CLSC (from "start")
-		HashMap<Clsc, ClscNode> map = new HashMap<Clsc, ClscNode>();
-		
+		HashMap<Clsc, ClscNode> map = new HashMap<Clsc, ClscNode>();		
 		ArrayList<Clsc> visitedList = new ArrayList<Clsc>();
-		// We initialize our Hashmap
-		for (Clsc point : clscArray_) {	
-			map.put(point, new ClscNode(point));
-		}
-		// The starting Node is set to 0 and all the rest is set to infinity by default
-		map.get(start).setTime_(0);
-		map.get(start).addToPath(start);
-		visitedList.add(start);	// We first visit the starting CLSC
+		// We initialize our Hashmap and our ArrayList, and we also put start in the list
+		initialize(map, visitedList, start);
 		
 		int bestTime = Dijkstra(map, visitedList, start, destination, vehicle);
 		
-		if (bestTime != Integer.MAX_VALUE) {	// If no errors
+		/// TODO: Instead of the current code, put String parts in the main and only return the vehicle? (what to do with map?)
+		if (bestTime != Integer.MAX_VALUE) {	// If no errors with the NI-NH vehicle
 			builder.append("Shortest path to CLSC #" + destination.getId() + " from CLSC #" + start.getId() + " :\n"
 					+ "\tVehicle type : NI-NH\n"
-					+ "\tBattery remaining : " + vehicle.chargeLeft()
+					+ "\tBattery remaining : " + String.format("%.2f", vehicle.chargeLeft()) + "%"
 					+ "\tShortest path :\n\t");
 			for (Clsc clsc : map.get(destination).getPathToPoint_()) {
-				
+				builder.append(" -> " + clsc.getId());
 			}
-			
+			builder.append("\n\t\t(in " + bestTime + " minutes)");
+			return builder.toString();
 		}
-		return shortestPath;
+		// If the NI-NH vehicle failed, we must try with the LI-ion vehicle
+		vehicle.switchType();
+		// We also reset our containers and try again
+		initialize(map, visitedList, start);
+		bestTime = Dijkstra(map, visitedList, start, destination, vehicle);
+		
+		if (bestTime != Integer.MAX_VALUE) {	// If a path has been found with the LI-ion vehicle
+			// Same code as above... Only Vehicle type changes
+			builder.append("Shortest path to CLSC #" + destination.getId() + " from CLSC #" + start.getId() + " :\n"
+					+ "\tVehicle type : LI-ion\n"
+					+ "\tBattery remaining : " + String.format("%.2f", vehicle.chargeLeft()) + "%"
+					+ "\tShortest path :\n\t");
+			for (Clsc clsc : map.get(destination).getPathToPoint_()) {
+				builder.append(" -> " + clsc.getId());
+			}
+			builder.append("\n\t\t(in " + bestTime + " minutes)");
+			return builder.toString();
+		}
+		// If both of the vehicles failed, then the move is impossible
+		builder.append("ERROR : The move from CLSC #" + start.getId() + " to CLSC #" + destination.getId()
+		+ " is impossible with the NI-NH vehicle nor the LI-ion vehicle because of :"
+		+ "\t1) Not enough battery charge and/or no charging stations available or\n"
+		+ "\t2) Invalid CLSC");	// TODO: Elaborate
+		return builder.toString();
+	}
+	
+	private void initialize(HashMap<Clsc, ClscNode> mapToInitialize, ArrayList<Clsc> listToInitialize, Clsc start) {
+		// The content is reset
+		mapToInitialize.clear();
+		listToInitialize.clear();
+		
+		// The keys are initialized and the corresponding node as well
+		for (Clsc point : clscArray_) {	
+			mapToInitialize.put(point, new ClscNode(point));
+		}
+		// The starting Node is set to 0 and all the rest is set to infinity by default
+		mapToInitialize.get(start).setTime_(0);
+		mapToInitialize.get(start).addToPath(start);
+		listToInitialize.add(start);	// We first visit the starting CLSC
 	}
 	
 	/* 1- We find the shortest CLSC not in visited
@@ -77,6 +110,10 @@ public class Graph {
 		if (nextClsc == destination) {
 			return map.get(nextClsc).getTime_();	// Can also access to the path
 		}
-		return Dijkstra(map, visitedList, nextClsc, destination);
+		int minutesTaken;
+		while (vehicle.chargeLeft())
+		minutesTaken = Dijkstra(map, visitedList, nextClsc, destination, vehicle);
+		
+		return minutesTaken;
 	}
 }
--- Vehicle.java
+++ Vehicle.java
@@ -2,23 +2,25 @@
  * Vehicle : Stores battery value and checks if a move is possible
  */
 public enum Vehicle { 
-	LOW_RISK(6, 5), 
-	MEDIUM_RISK(12, 10), 
-	HIGH_RISK(48, 30);
-	private final int NimhMultiplier_;
-	private final int LiionMultiplier_;
+	// The multipliers have been converted from %/h in %/min (rounded)
+	// ex: 6%/h = 6%/(min*60) = 0.1%/min
+	LOW_RISK(0.1, 0.083), 
+	MEDIUM_RISK(0.2, 0.167), 
+	HIGH_RISK(0.8, 0.5);
+	private final double NimhMultiplier_;
+	private final double LiionMultiplier_;
 	
-	private int battery_;
-	private int multiplier_;
+	private double battery_;
+	private double multiplier_;
 	
-	Vehicle(int mult1, int mult2) {
+	Vehicle(double mult1, double mult2) {
 		NimhMultiplier_ = mult1;
 		LiionMultiplier_ = mult2;
 		battery_ = 100;
 		multiplier_ = NimhMultiplier_;
 	}
 	
-	public int chargeLeft() {
+	public double chargeLeft() {
 		return battery_;
 	}
 	
@@ -28,16 +30,21 @@ public enum Vehicle {
 	
 	/* Calculates the remaining charge of the battery after timeTaken of travel.
 	 * Returns the remaining charge and updates it if > 20%; if not, returns Integer.MAX_VALUE
+	 * Note: timeTaken is in MINUTES
 	 */
-	public int travelFor(int timeTaken) {
-		int remainingBattery = battery_ - timeTaken * multiplier_;
+	public double travelFor(int timeTaken) {
+		double remainingBattery = battery_ - timeTaken * multiplier_;
 		if (remainingBattery < 20)
 			return Integer.MAX_VALUE;
 		battery_ = remainingBattery;
 		return battery_;
 	}
 	
+	/*
+	 * Switches from NI-NH to LI-ion. The battery is reset to full.
+	 */
 	public void switchType() {
 		multiplier_ = LiionMultiplier_;
+		battery_ = 100;
 	}
 };
