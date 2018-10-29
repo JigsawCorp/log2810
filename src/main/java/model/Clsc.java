0094020548566d1d653fe1c39e65c8d35631fffd
Baboo <philbond1997@hotmail.com>
2018-10-29 12:15:32 -0400

    Commented Clsc.java

--- src/main/java/model/Clsc.java
+++ src/main/java/model/Clsc.java
@@ -1,49 +1,51 @@
 package main.java.model;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Clsc {
-	
-	int id_;
-	boolean hasTerminal_;
-	//ArrayList<Path> fPaths;
-	//ArrayList<Clsc> fNeigbourghs;
+/**
+ * Represents a Graph node.
+ */
+public class CLSC {
 
-	HashMap<Clsc, Integer> fNeigbourghs;
-	
-	public Clsc(int id, boolean hasTerminal) {
-		id_ = id;
-		hasTerminal_ = hasTerminal;
-		fNeigbourghs = new HashMap<>();
+    // Holds the ID of the CLSC. Starting from 1 to infinity.
+	private int fID;
+	// Is true if the CLSC has a charging terminal.
+	private boolean fHasTerminal;
+	// Holds all the neighbors of a CLSC node. The key is the neighbor CLSC and the value is the distance in minutes.
+	private HashMap<CLSC, Integer> fNeighbors;
 
+    /**
+     * Constructor
+     * @param id The id of the CLSC
+     * @param hasTerminal True if the CLSC has a charging terminal
+     */
+	CLSC(int id, boolean hasTerminal) {
+		fID = id;
+		fHasTerminal = hasTerminal;
+		fNeighbors = new HashMap<>();
 	}
-	
-	public int getId() {
-		return id_;
-	}
-	
-	public boolean hasCharge() {
-		return hasTerminal_;
-	}
-	
-	//void addPath(Path path) {
-	//	fPaths.add(path);
-	//}
 
-	//public ArrayList<Clsc> getNeigbourghs() {
-	//	return fNeigbourghs;
-	//}
-
-	//public ArrayList<Path> getPaths() {
-	//	return fPaths;
-	//}
-
-	public HashMap<Clsc, Integer> getNeigbourghs() {
-		return fNeigbourghs;
+    /**
+     * Getter of fHasTerminal
+     * @return fHasTerminal
+     */
+	boolean hasCharge() {
+		return fHasTerminal;
 	}
-	
+
+    /**
+     * Getter of fNeighbors
+     * @return fNeighbors
+     */
+	public HashMap<CLSC, Integer> getNeighbors() {
+		return fNeighbors;
+	}
+
+    /**
+     * Formats a CLSC into a String
+     * @return A formatted String
+     */
 	public String toString() {
-		return "CLSC" + id_ + ", " + hasTerminal_ + "\n";
+		return "CLSC" + fID + ", " + fHasTerminal + "\n";
 	}
 }
