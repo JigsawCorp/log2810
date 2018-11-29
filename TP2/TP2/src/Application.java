216befe365e5ea726936e4e9c43644ca06f08a22
Phil <philbond1997@hotmail.com>
2018-11-29 16:09:23 -0500

    Finished lexicon

--- TP2/TP2/src/Application.java
+++ TP2/TP2/src/Application.java
@@ -1,5 +1,7 @@
 public class Application {
-    public static void start() {
+    private static final String DEFAULT_PATH = "/TP2/lexiques/";
 
+    public void start() {
+        Lexicon lexicon = Lexicon.newLexicon(DEFAULT_PATH + "lexique 1.txt");
     }
 }
