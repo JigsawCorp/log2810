6b61717380b13bf110b1d1fcfbd68ce63b85807c
Phil <philbond1997@hotmail.com>
2018-11-29 13:11:38 -0500

    Started  Lexicon

--- TP2/TP2/src/Lexicon.java
+++ TP2/TP2/src/Lexicon.java
@@ -1,7 +1,12 @@
+import com.sun.tools.javac.Main;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -9,13 +14,37 @@ import java.util.Queue;
  */
 public class Lexicon {
 	
-	private HashMap<Character,Node> startStates;
+	private Map<Character, Node> startStates;
 	private Node currentNode;
 	private Queue<Node> top5;
+
+	public Lexicon() {
+	    top5 = new LinkedList<>();
+	    startStates = new HashMap<>();
+    }
 	
-	public Lexicon(String file) {
-		
+	public static Lexicon newLexicon(String file) {
+		Lexicon lexicon = new Lexicon();
+        InputStream inputStream = Main.class.getResourceAsStream(file);
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+            String currentLine;
+
+            while (!(currentLine = br.readLine()).isEmpty()) {
+                Queue<Character> string = new LinkedList<>();
+                for (int i = 0; i < currentLine.length(); ++i) {
+                    string.offer(currentLine.charAt(i));
+                }
+                lexicon.addState(string);
+            }
+
+
+        } catch (IOException e) {
+            // Pour s'assurer que le fichier n'est pas vide.
+            e.printStackTrace();
 	}
-	
+
 	
 }
